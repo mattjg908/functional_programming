8a6de0158bebb8f96fb436caeb200f17a89eda8c
Matt Galvin <matthewgalvin908@gmail.com>
2020-01-24 20:53:11 +0100

    Ch.2, exercise 1- calculate cost of groceries

---
+++ work_with_functions/lib/chapter_2/exercises.ex
@@ -0,0 +1,28 @@
+defmodule Chapter2.Exercises do
+  @moduledoc """
+  This module is for the exercises at the end of Chapter 2
+  """
+
+  @price_of_bread 0.1
+  @price_of_milk 2.0
+  @price_of_cake 15.0
+
+  @doc """
+  Create an expression that solves the following problem:
+
+  Sarah has bought ten slices of bread for ten cents each, three bottles of
+  milk for two dollars each, and a cake for fifteen dollars. How many dollars
+  has Sarah spent?
+
+  """
+  def total_money_spent(slice_quant, bottle_quant, cake_quant) do
+    money_spent_on_bread = fn slices_bought -> slices_bought * @price_of_bread end
+    money_spent_on_milk = fn bottles_bought -> bottles_bought * @price_of_milk end
+    money_spent_on_cake = fn cakes_bought -> cakes_bought * @price_of_cake end
+
+    money_spent_on_bread.(slice_quant)
+    |> Kernel.+(money_spent_on_milk.(bottle_quant))
+    |> Kernel.+(money_spent_on_cake.(cake_quant))
+  end
+
+end
